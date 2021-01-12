90aa38e64b13cac2f65071fdeac9e98b805648f0
Marco Cardia <cardiamc@gmail.com>
2021-01-12 23:56:21 +0100

    Cache functions

---
+++ utils/cache.py
@@ -0,0 +1,37 @@
+import h5py
+import pickle
+
+def read_matrix_from_cache(fname, scaler_file):
+    mmn = pickle.load(open(scaler_file, 'rb'))
+
+    f = h5py.File(fname, 'r')
+    num = int(f['num'].value)
+    X_train, Y_train, X_test, Y_test = [], [], [], []
+    for i in range(num):
+        X_train.append(f['X_train_%i' % i].value)
+        X_test.append(f['X_test_%i' % i].value)
+    Y_train = f['Y_train'].value
+    Y_test = f['Y_test'].value
+    external_dim = f['external_dim'].value
+    timestamp_train = f['T_train'].value
+    timestamp_test = f['T_test'].value
+    f.close()
+
+    return X_train, Y_train, X_test, Y_test, mmn, external_dim, timestamp_train, timestamp_test
+
+def save_matrix_in_cache(fname, X_train, Y_train, X_test, Y_test, external_dim, timestamp_train, timestamp_test):
+    h5 = h5py.File(fname, 'w')
+    h5.create_dataset('num', data=len(X_train))
+
+    for i, data in enumerate(X_train):
+        h5.create_dataset('X_train_%i' % i, data=data)
+    # for i, data in enumerate(Y_train):
+    for i, data in enumerate(X_test):
+        h5.create_dataset('X_test_%i' % i, data=data)
+    h5.create_dataset('Y_train', data=Y_train)
+    h5.create_dataset('Y_test', data=Y_test)
+    external_dim = -1 if external_dim is None else int(external_dim)
+    h5.create_dataset('external_dim', data=external_dim)
+    h5.create_dataset('T_train', data=timestamp_train)
+    h5.create_dataset('T_test', data=timestamp_test)
+    h5.close()
