352f0b4c54341964b681d0b44aa6299ee2617afc
cardiamc <cardiamc@gmail.com>
2022-01-16 19:33:56 +0100

    setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='AdjNet',
-      version='1.0',
-      packages=['AdjNet'],
-      package_dir={'', 'AdjNet'}
-      )
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="AdjNet",
+    version="0.0.1",
+    author="Jeff Hale",
+    author_email="jeffmshale@gmail.com",
+    description="A package to convert your Jupyter Notebook",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonpappalord/crowd_flow_prediction",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
