61b9e42ed0181028554bc52e2dda6e46944f0afe
cardiamc <cardiamc@gmail.com>
2022-01-16 19:36:53 +0100

    setup.py file

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+requirements = []
 
 setup(
     name="AdjNet",
     version="0.0.1",
-    author="Jeff Hale",
-    author_email="jeffmshale@gmail.com",
-    description="A package to convert your Jupyter Notebook",
+    author="Marco Cardia",
+    author_email="m.cardia@studenti.unipi.it",
+    description="Python implementation of the thesis AdjNet: a deep learning approach for Crowd Flow Prediction",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/jonpappalord/crowd_flow_prediction",
