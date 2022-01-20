62b2edbb80824d7de59e89251b830b9afff31a39
cardiamc <cardiamc@gmail.com>
2022-01-20 19:28:45 +0100

    Updated dataset.py

--- src/AdjNet/dataset.py
+++ src/AdjNet/dataset.py
@@ -4,12 +4,14 @@ import torch
 
 class Dataset(torch.utils.data.Dataset):
     
-    def __init__(self, data, n_his):
+    def __init__(self, data, n_his, transform=None, target_transform=None):
         super().__init__()
         self.data = data
         self.mean = np.mean(data)
         self.std = np.std(data)
         self.n_his = n_his
+        self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
         'Denotes the total number of samples'
@@ -27,5 +29,14 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
+        
+        hist_data = self.data[idx, :, :self.n_his]
+        label_data = self.data[idx, :, self.n_his:]
 
-        return self.data[idx, :, :self.n_his],  self.data[idx, :, self.n_his:]
+        if self.transform:
+            hist_data = self.transform(hist_data)
+
+        if self.target_transform:
+            label_data = self.target_transform(label_data)
+
+        return hist_data, label_data
