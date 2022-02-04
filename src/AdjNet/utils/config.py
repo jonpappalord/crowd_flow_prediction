2600ef3ef00e3cebc5d7262d4ad838c02590a248
cardiamc <cardiamc@gmail.com>
2022-02-04 16:00:13 +0100

    Config file changed

--- src/AdjNet/utils/config.py
+++ src/AdjNet/utils/config.py
@@ -2,21 +2,29 @@ from __future__ import print_function
 import os
 import platform
 
-class Config(object):
+class Config_setup(object):
     """docstring for Config"""
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Config_setup, cls).__call__(*args, **kwargs)
+        else:
+            cls._instances[cls].__init__(*args, **kwargs)
+        return cls._instances[cls]
 
-    def __init__(self):
+    def __init__(self, DATAPATH=None):
         super(Config, self).__init__()
 
-        DATAPATH = os.environ.get('DATAPATH')
         if DATAPATH is None:
-            if platform.system() == "Windows" or platform.system() == "Linux":
-                # DATAPATH = "D:/data/traffic_flow"
-            # elif platform.system() == "Linux":
-                DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../..', 'data')
-            else:
-                print("Unsupported/Unknown OS: ", platform.system, "please set DATAPATH")
+            DATAPATH = os.environ.get('DATAPATH')
+            if DATAPATH is None:
+                if platform.system() == "Windows" or platform.system() == "Linux":
+                    # DATAPATH = "D:/data/traffic_flow"
+                # elif platform.system() == "Linux":
+                    DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../..', 'data')
+                else:
+                    print("Unsupported/Unknown OS: ", platform.system, "please set DATAPATH")
         self.DATAPATH = DATAPATH
         
-# class Config(metaclass=Config_setup):
-#     pass
+class Config(metaclass=Config_setup):
+    pass
